65dae8e127041b2b77fe069ec4e863df8c9ad093
wsugg <sugg.will@gmail.com>
2013-02-16 09:31:35 -0800

    adding signing in as admin first on edit and delete project_specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 
 describe ProjectsController do
    let(:user) { Factory(:confirmed_user) }
+   let(:project) { mock_model(Project, :id => 1) }
    
    context "standard users" do
     before do
      sign_in(:user, user)
    end
  
-   it "cannot access the new action" do
-     get :new
-     response.should redirect_to('/')
-     flash[:alert].should == "You must be an admin to do that."
+  { :new => :get,
+    :create => :post,
+    :edit => :get,
+    :update => :put,
+    :destroy => :delete }.each do |action, method|
+   it "cannot access the #{action} action" do
+    sign_in(:user, user)
+    send(method, action, :id => project.id)
+    response.should redirect_to(root_path)
+    flash[:alert].should eql("You must be an admin to do that.") 
     end
-  end
+   end
   
   it "displays an error for a missing project" do
    get :show, :id => "not-here"
@@ -21,4 +28,5 @@ describe ProjectsController do
    message = "The project you were looking for could not be found."
    flash[:alert].should == message
   end
+end
 end
--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
  feature "Editing Projects" do
-  before do  
+  before do
+    sign_in_as!(Factory(:admin_user))
     Factory(:project, :name => "TextMate 2")
     visit "/"
     click_link "TextMate 2"
