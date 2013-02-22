415f461b665cc3d4abd5547691e595323e956f42
wsugg <sugg.will@gmail.com>
2013-02-22 12:53:51 -0800

    completing hiding links for non admin users

--- spec/integration/hidden_links_spec.rb
+++ spec/integration/hidden_links_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 feature "hidden links" do
   let(:user) { Factory(:confirmed_user) }
   let(:admin) { Factory(:admin_user) }
+  let(:project) { Factory(:project) }
+
   
   context "anonymous users" do
    scenario "cannot see the New Project link" do
     visit '/'
     assert_no_link_for "New Project"
+   end
+
+   scenario "cannot see the Edit Project link" do
+    visit project_path(project)
+    assert_no_link_for "Edit Project"
+   end
+
+   scenario "cannot see the Delete Project link" do
+    visit project_path(project)
+    assert_no_link_for "Delete Project"
    end
   end
 
@@ -17,6 +29,16 @@ feature "hidden links" do
       visit '/'
       assert_no_link_for "New Project"
      end
+
+     scenario "cannot see the Edit Project link" do
+      visit project_path(project)
+      assert_no_link_for "Edit Project"
+     end
+
+     scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      assert_no_link_for "Delete Project"
+     end
     end
 
    context "admin users" do
@@ -24,6 +46,16 @@ feature "hidden links" do
      scenario "can see the New Project link" do
       visit '/'
       assert_link_for "New Project"
+     end
+
+     scenario "can see the Edit Project link" do
+      visit project_path(project)
+      assert_link_for "Edit Project"
+     end
+
+     scenario "can see the Delete Project link" do
+      visit project_path(project)
+      assert_link_for "Delete Project"
      end
    end
 end
