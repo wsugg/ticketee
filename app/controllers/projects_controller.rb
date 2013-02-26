28b81e6d70dd32fe9abf8e6754c60c93217ba7bf
wsugg <sugg.will@gmail.com>
2013-02-26 14:27:17 -0800

    fixed find_project in projects controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,10 +52,10 @@ class ProjectsController < ApplicationController
 
 	private
 	  def find_project
-        @project = Project.for(current_user).find(params[:project_id])
+        @project = Project.for(current_user).find(params[:id])
 	    rescue ActiveRecord::RecordNotFound
 		flash[:alert] = "The project you were looking" +
 		                " for could not be found."
-		redirect_to root_path
+		redirect_to projects_path
 	  end
 end
