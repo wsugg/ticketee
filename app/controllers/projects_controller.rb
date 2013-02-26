8e2a7851e7335273b75f0fa366decdb83dd60459
wsugg <sugg.will@gmail.com>
2013-02-26 12:41:29 -0800

    users can only see links they are supposed to

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   before_filter :find_project, :only => [:show,:edit,:update,:destroy]
 	
 	def index
-	    @projects = Project.all
+	    @projects = Project.for(current_user).all
 	end
 
 	def new
@@ -52,11 +52,7 @@ class ProjectsController < ApplicationController
 
 	private
 	  def find_project
-	    @project = if current_user.admin?
-         Project.find(params[:id])
-        else
-         Project.viewable_by(current_user).find(params[:id])
-        end
+        @project = Project.for(current_user).find(params[:id])
 	    rescue ActiveRecord::RecordNotFound
 		flash[:alert] = "The project you were looking" +
 		                " for could not be found."
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,19 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name
-
+  
   validates :name, :presence => true, :uniqueness => true
- 
+
   has_many :permissions, :as => :thing
-   def self.viewable_by(user)
-    joins(:permissions).where(:permissions => { :action => "view",
-                                                :user_id => user.id })
-   end
-
   has_many :tickets, :dependent => :delete_all
-
+  
   scope :admins, where(:admin => true)
+
+  def self.viewable_by(user)
+   joins(:permissions).where(:permissions => { :action => "view",
+                                               :user_id => user.id })
+  end
+
+  def self.for(user)
+   user.admin? ? Project : Project.viewable_by(user)
+  end
 end
