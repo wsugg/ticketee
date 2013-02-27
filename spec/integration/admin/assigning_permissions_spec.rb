a87ac57d1fe3da74251efcc61c769bbfdeed389b
wsugg <sugg.will@gmail.com>
2013-02-27 13:40:36 -0800

    adding remaining perms to perms helper.

--- spec/integration/admin/assigning_permissions_spec.rb
+++ spec/integration/admin/assigning_permissions_spec.rb
@@ -35,6 +35,32 @@ feature "Assigning permissions" do
   click_button "Create"
   page.should have_content("Ticket has been created.")
  end
+
+ scenario "Updating a ticket for a project" do
+   check_permission_box "view", project
+   check_permission_box "edit_tickets", project
+   click_button "Update"
+   click_link "Sign out"
+   sign_in_as!(user)
+   click_link project.name
+   click_link ticket.title
+   click_link "Edit Ticket"
+   fill_in "Title", :with => "Really shiny!"
+   click_button "Update Ticket"
+   page.should have_content("Ticket has been updated")
+ end
+
+ scenario "Deleting a ticket for a project" do
+  check_permission_box "view", project
+  check_permission_box "delete_tickets", project
+  click_button "Update"
+  click_link "Sign out"
+  sign_in_as!(user)
+  click_link project.name
+  click_link ticket.title
+  click_link "Delete Ticket"
+  page.should have_content("Ticket has been deleted.")
+ end
 end
 
 
