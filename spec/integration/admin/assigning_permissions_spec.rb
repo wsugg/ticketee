e9eebf750649578a934644963e4d93cf6f5d92c8
wsugg <sugg.will@gmail.com>
2013-02-27 13:11:50 -0800

    adding Create Tickets perms

--- spec/integration/admin/assigning_permissions_spec.rb
+++ spec/integration/admin/assigning_permissions_spec.rb
@@ -21,6 +21,20 @@ feature "Assigning permissions" do
   sign_in_as!(user)
   page.should have_content(project.name)
  end
+
+ scenario "Creating tickets for a project" do
+  check_permission_box "view", project
+  check_permission_box "create_tickets", project
+  click_button "Update"
+  click_link "Sign out"
+  sign_in_as!(user)
+  click_link project.name
+  click_link "New Ticket"
+  fill_in "Title", :with => "Shiny!"
+  fill_in "Description", :with => "Make it so!"
+  click_button "Create"
+  page.should have_content("Ticket has been created.")
+ end
 end
 
 
